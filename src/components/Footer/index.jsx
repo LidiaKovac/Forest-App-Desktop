58aa3a4cfaff47cb7320daa94dbc378fa44cea1d
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-04-24 22:11:50 +0200

    adds footer with disclaimer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+
+import "./index.scss";
+
+const Footer = ()=> {
+    return (<div className="footer__wrap">
+        This is not an official Forest App. This is an open source project, please leave feedback on <a rel="noreferrer" target='_blank' href='https://github.com/LidiaKovac/Forest-App-Desktop'>GitHub</a> or on <a rel="noreferrer" target='_blank' href="https://www.instagram.com/lidiacodes/">Instagram</a>
+    </div>)
+}
+export default Footer;
