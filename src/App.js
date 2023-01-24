87873927f73d5dea741a86dbe1aba2e4cfe5a614
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-01-24 15:56:20 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-import { Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import Home from './views/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Route exact path="/">
+    <>
       <Home/>
       <Footer/>
-    </Route>
-    </BrowserRouter>
+      </>
   );
 }
 
